4240384d6765c7e695dfb1beb03e23593e1334cb
andersoncandid <190989879+andersoncandid@users.noreply.github.com>
2025-11-06 05:02:49 -0300

    Completed exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let arr = [];
 
+  // Check if inputs are posite integer numbers
+  if (typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+
+  // In case of larger number first
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  for (i = a; i <= b; i++) {
+    arr.push(i);
+  }
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
 // Do not edit below this line
