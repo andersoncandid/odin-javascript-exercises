b3708988ea317d2c2be2ffef892686dbe1c665eb
andersoncandid <190989879+andersoncandid@users.noreply.github.com>
2025-11-06 04:32:50 -0300

    Completed exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...rest) {
+  return arr.filter((item, index, array) => !rest.includes(item));
 };
 
 // Do not edit below this line
