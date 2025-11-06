9577883173cf63fc8b8793ce734229e5ae8afec2
andersoncandid <190989879+andersoncandid@users.noreply.github.com>
2025-11-06 03:37:33 -0300

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let strSize = str.length;
+  let newStr = "";
 
+  for (let i = 1; i <= strSize; i++) {
+    let char = str.charAt(strSize - i);
+    newStr += char;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
